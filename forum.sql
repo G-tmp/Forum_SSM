52a0d86e203914ece8e2f4df8020bb83d8a129d0
G-tmp <karen-noreply@protonmail.com>
2020-05-10 10:17:02 +0800

    add SQL

---
+++ forum.sql
@@ -0,0 +1,113 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Linux (x86_64)
+--
+-- Host: localhost    Database: forum
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `block`
+--
+
+DROP TABLE IF EXISTS `block`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `block` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL COMMENT '名称',
+  `ename` varchar(100) NOT NULL COMMENT '英文名',
+  `description` varchar(500) NOT NULL DEFAULT '' COMMENT '描述',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `ename` (`ename`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL COMMENT '标题',
+  `content` varchar(1000) NOT NULL DEFAULT '' COMMENT '内容',
+  `hit` int(11) NOT NULL DEFAULT '0' COMMENT '点击',
+  `votes` int(11) NOT NULL DEFAULT '0' COMMENT '投票数',
+  `reply_count` int(11) NOT NULL DEFAULT '0' COMMENT '回复数',
+  `user_id` int(11) NOT NULL COMMENT '用户_id',
+  `block_id` int(11) NOT NULL COMMENT '板块_id',
+  `publish_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '发布时间',
+  `last_reply_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '最新回复时间',
+  `is_banned` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否被ban',
+  `reported` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `reply`
+--
+
+DROP TABLE IF EXISTS `reply`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reply` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` mediumtext NOT NULL,
+  `user_id` int(11) NOT NULL COMMENT '用户_id',
+  `post_id` int(11) NOT NULL COMMENT '帖子_id',
+  `reply_id` int(11) NOT NULL DEFAULT '0' COMMENT '回复回复',
+  `floor` int(10) unsigned NOT NULL DEFAULT '0',
+  `publish_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '发布时间',
+  `is_banned` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否被ban',
+  `reported` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL COMMENT '邮箱',
+  `password` varchar(50) NOT NULL COMMENT '密码',
+  `nickname` varchar(50) NOT NULL COMMENT '昵称',
+  `profile` varchar(50) NOT NULL DEFAULT '' COMMENT '头像',
+  `bio` varchar(500) NOT NULL DEFAULT '' COMMENT '自我介绍',
+  `is_banned` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否被封',
+  `is_admin` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否管理员',
+  `register_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '注册时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `nickname` (`nickname`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-10  9:59:39
